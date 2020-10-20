e5ec92af506ca2967bb1745122b7db037f58f1cf
Steveen <steveen.echeverri@gmail.com>
2020-10-19 23:11:42 -0500

    Add data info service social footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPageService } from 'src/app/services/info-page.service';
 
 @Component({
   selector: 'app-footer',
@@ -9,7 +10,7 @@ export class FooterComponent implements OnInit {
 
   year: number = new Date().getFullYear();
 
-  constructor() { }
+  constructor( public _service: InfoPageService ) { }
 
   ngOnInit(): void {
   }
